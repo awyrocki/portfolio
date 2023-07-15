eefe665bc6fa81aacf9de68b5cbd82d776dbb766
Austin Wyrocki <austinwyrocki@gmail.com>
2023-07-15 16:22:09 -0400

    contact info

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import "./Contact.css"
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div id='contact-div'>
+      <h1 id='h1'>Austin Wyrocki</h1>
+      <div id='contact-div2'>
+        <span id='contact-span'><a href='https://www.linkedin.com/in/austin-wyrocki/'>Linkedin</a></span>
+        <span id='contact-span'><a href="https://github.com/awyrocki">GitHub</a></span>
+      </div>
+    </div>
   )
 }
 
--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./Work.css"
 
 function Work() {
   return (
