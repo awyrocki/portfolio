238115cdb29c6f107e72dbc9885a8600e7dc84ac
Austin Wyrocki <austinwyrocki@gmail.com>
2023-08-23 15:30:10 -0400

    removed light/dark, added experience section

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { React, useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import Projects from './components/Projects/Projects'
 import NotFound from './components/NotFound/NotFound'
 import About from './components/About/About'
 import Experience from './components/Experience/Experience'
 import darkBackground from "./dark.svg"
-import lightBackground from "./light.svg"
 
 
 function App() {
   return (
     <>
-      <div id='background-div' style={{backgroundImage: `url(${darkBackground})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center"}}>
+      <div id='background-div' 
+        style={{backgroundImage: `url(${darkBackground})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover'
+      }}>
       <Router>
       <Nav />
       <Routes>
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,6 +4,7 @@ import "./About.css";
 
 function About() {
   return (
+    <>
     <div id='about-div'>
       <img id='about-img' src={brox} alt="" />
       <div id='bio-div'>
@@ -13,6 +14,26 @@ function About() {
         <span id='about-bio'>My diverse background, combined with my team-oriented approach, analytical abilities, and customer-facing experience, make me a valuable asset in any collaborative and innovative software development environment. Let's connect and explore how we can work together to create impactful solutions.</span><h></h>
       </div>
     </div>
+      <div id='experience-container'>
+        <div id='job1-container'>
+          <div id='job1-dates'>
+          <span>Autumn Harp, Essex VT</span>
+          <span>09-2021 - 05-2023</span>
+          </div>
+          <div id='job1-description'>
+            <div id='job-title'>
+            <span>Formulation Chemist</span>
+            </div>
+            <div id='job1-role'>
+            <span>Utilized customer feedback to develop, refine, troubleshoot and deploy personal care products for market, all while adhering to practical budget and timeline</span>
+            </div>
+            <div id='job1-skills'>
+            <span>Skills</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
