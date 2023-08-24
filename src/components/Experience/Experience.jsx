0dcafc3ded664b98d44677d2144ec7840bc2965d
Austin Wyrocki <austinwyrocki@gmail.com>
2023-08-24 10:29:43 -0400

    reworked file structure

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
-import "./Experience.css"
+import React from 'react';
+import brox from "./brox.png";
+import "./Experience.css";
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
 
 function Experience() {
   return (
-    <div>Experience</div>
+    <>
+    <div id='about-div'>
+      <img id='about-img' src={brox} alt="" />
+      <div id='bio-div'>
+        <h></h>
+        <span id='about-bio'>Experienced Chemist turned Software Developer with a passion for creating innovative solutions. Throughout my career, I have honed my skills in both scientific research and software development, combining my expertise to deliver cutting-edge solutions. I thrive in collaborative environments and have a strong commitment to teamwork, recognizing the value of diverse perspectives and effective communication.</span>
+        <span id='about-bio'>As a lifelong learner, I remain up-to-date with the latest technologies and trends in the software development industry. This commitment to continuous growth enables me to adapt quickly and efficiently to new challenges, ensuring that my solutions are always at the forefront of innovation. I possess a strong analytical mindset, allowing me to effectively analyze and debug code, providing well-researched and cost-effective answers to complex problems.</span>
+        <span id='about-bio'>My diverse background, combined with my team-oriented approach, analytical abilities, and customer-facing experience, make me a valuable asset in any collaborative and innovative software development environment. Let's connect and explore how we can work together to create impactful solutions.</span><h></h>
+      </div>
+    </div>
+      <div id='experience-container'>
+        <div id='job1-container'>
+          <div id='job1-dates'>
+          <span>Sep-2021 - May-2023</span>
+          </div>
+          <div id='job1-description'>
+            <div id='job-title'>
+            <span>Autumn Harp, Formulation Chemist</span>
+            </div>
+            <div id='job1-role'>
+            <span>Utilized customer feedback to develop, refine, troubleshoot and deploy personal care products for market, all while adhering to practical budget and timeline</span>
+            </div>
+            <div id='job1-skills'>
+              <Stack sx={{mt: 1}} direction="row" spacing={1}>
+                <Chip label="Project Management" variant="outlined" />
+                <Chip label="Scrum Methodology" variant="outlined" />
+                <Chip label="Experimental Design" variant="outlined" />
+              </Stack>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
