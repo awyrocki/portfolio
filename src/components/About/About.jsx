618cda0cd4b18a3dedcfacffabda7d111790f0cc
Austin Wyrocki <austinwyrocki@gmail.com>
2023-09-02 17:44:07 -0400

    404 page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,8 +6,8 @@ function About() {
   return (
     <>
     <Box height={"80vh"} display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
-      <Typography fontFamily={'Century Gothic'} variant="h2" textAlign={'center'} color={"#e0e0e0"} sx={{mb: 5}}>Austin Wyrocki</Typography>
-      <Typography fontFamily={'Geneva'} variant='h4' textAlign={'center'} color={"#e0e0e0"}>Full-stack Developer</Typography>
+      <Typography fontFamily={'Century Gothic'} variant="h2" color={"#e0e0e0"} sx={{mb: 5}}>Austin Wyrocki</Typography>
+      <Typography fontFamily={'Geneva'} variant='h4' color={"#e0e0e0"}>Full-stack Developer</Typography>
     </Box>
     </>
   )
--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import "./NotFound.css"
+import { Box, Typography } from "@mui/material"
 
 function NotFound() {
   return (
-    <div>Not found page</div>
+    <>
+    <Box height={"80vh"} justifyContent={'center'} alignItems={'center'} display={'flex'}>
+      <Typography fontFamily={'Geneva'} variant='h3' color={'#e0e0e0'}>Page not found</Typography>
+    </Box>
+    </>
   )
 }
 
