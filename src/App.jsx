7ebaaab13e099642d5607c9132ba219ff27656d1
Austin Wyrocki <austinwyrocki@gmail.com>
2023-09-02 22:11:29 -0400

    fixed?

--- src/App.jsx
+++ src/App.jsx
@@ -49,10 +49,10 @@ function App() {
     </nav>
     </div>
       <Routes>
-        <Route path='*' element={ <NotFound /> } />
         <Route path='/' element={ <About /> } />
         <Route path='/Experience' element={ <Experience /> } />
         <Route path='/Projects' element={ <Projects /> } />
+        <Route path='*' element={ <NotFound /> } />
       </Routes>
     </>
   );
